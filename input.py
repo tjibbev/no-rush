07808062f7d999d68e5d05683170a1b52360f667
tjibbev <tjibbevalkenburg@gmail.com>
2022-01-11 15:00:03 +0100

    Edited input csv board

--- input.py
+++ input.py
@@ -1,1 +1,21 @@
 # Het input gedeelte, initialisatie programma.
+
+import argparse
+import os
+import sys
+
+
+if __name__ == "__main__":
+    # Argument parser
+    parser = argparse.ArgumentParser(description='Solve Rush Hour.')
+    parser.add_argument("board", type=int, help="The board to be solved.")
+
+    # Parse the command line arguments
+    args = parser.parse_args()
+
+    board_path = f"gameboards/{args.board}.csv"
+
+    # If the puzzle does not exist, exit
+    if not os.path.exists(board_path):
+        print(f"puzzle {args.board} does not exist")
+        sys.exit(1)
