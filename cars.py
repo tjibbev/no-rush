c33a69534286021ad6da4a161ac9bf8a9612e54c
root <root@LAPTOP-F5I6K527.localdomain>
2022-01-11 17:08:21 +0100

    wrote cars class

--- cars.py
+++ cars.py
@@ -1,1 +1,17 @@
 # Auto eigenschappen
+
+class Car:
+
+    def __init__(self, id, orientation, column, row, length) -> None:
+        self._id = id
+        self._orientation = orientation
+        self._coord = (int(row), int(column))
+        self._length = int(length)
+
+    def move(self, move):
+        if self._orientation == 'H':
+            self._coord = self._coord + (0, move) 
+        else:
+            self._coord = self._coord + (move, 0)
+
+    
