b1903e04bf6af0a28e1afd4567a248a201c5e395
Doena van den Hoven <doenavandenhoven@MacBook-van-Doena.local>
2022-01-20 18:01:32 +0100

    now runs alg N times

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import sys
 from code.classes.board import Board
 from code.algorithms.cl_player import test_game
 from code.algorithms.random_algo import random_traffic_control
+from code.algorithms.random_algo_long import random_traffic_control_long
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -13,11 +14,13 @@ if __name__ == "__main__":
     # Argument parser
     parser = argparse.ArgumentParser(description='Solve Rush Hour.')
     parser.add_argument("board", help="The board to be solved.")
+    parser.add_argument("N", help="The number of times to run the algorithm.")
 
     # Parse the command line arguments
     args = parser.parse_args()
 
     board_path = f"data/gameboards/{args.board}.csv"
+    N = int(args.N)
 
     # If the board does not exist, exit
     if not os.path.exists(board_path):
@@ -57,5 +60,17 @@ if __name__ == "__main__":
     # Game using command line
     # test_game(B)
 
+    sol_list = []
+    # let the best solution temporarily be 1500
+    best_sol = 1500
+
     # Random solution generator
-    random_traffic_control(B)
+    for i in range(N):
+        sol = random_traffic_control_long(B, i, best_sol)
+        sol_list.append(sol)
+        print(sol_list)
+        best_sol = int(min(sol_list))
+        print(best_sol)
+
+        # generate new board
+        B = Board(board_path, size)
