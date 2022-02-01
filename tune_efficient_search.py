62ee585f25c65d48883e4a2ee7cb9a0337d60c29
Doena van den Hoven <doenavandenhoven@MacBook-van-Doena.local>
2022-02-01 22:31:34 +0100

    clean up

--- tune_efficient_search.py
+++ tune_efficient_search.py
@@ -1,17 +1,17 @@
-""" Runs efficient_search.py for different filter values and creates a text file with the solutions"""
+""" Runs efficient_search.py for different filter values and creates a text file with the solution lengths"""
 """ The length of the file tells us how fast the algorithm is: the longer the faster """
 
 import subprocess
 import time
 
 # total running time for each filter value
-TOTAL_RUN_TIME = 10
+TOTAL_RUN_TIME = 60
 # the maximum time available per run 
-RUN_TIME = 5
+RUN_TIME = 20
 # chosen board
 BOARD = "Rushhour4x4_0"
 
-# the index of a value times 10 gives you the filter value
+# list of lengths of the solution length files per filter
 lengths_list = []
 
 for filter in range(0,100,10):
@@ -23,20 +23,24 @@ for filter in range(0,100,10):
             f = open(f"results_efficient_{filter}.txt", "a")
             subprocess.call(["timeout", f"{RUN_TIME}", "python3", "main.py", f"{BOARD}", "e", "--no-gif"], stdout=f)
             n_runs += 1
-        
         f.close()
+
+        # determine the length of the file
         fr = open(f"results_efficient_{filter}.txt", "r")
-        print("")
         length = len(fr.readlines())
-        lengths_list.append(length)
-        print(lengths_list)
-        print(f"number of solutions: {length}")
-        print("")
         fr.close()
 
+        # append to list and print 
+        lengths_list.append(length)
+        print(f"number of solutions: {length}")
+        print("")
+        
+# determine the best filter value,
+# the index of a value times 10 gives you the filter value
 max_value = max(lengths_list)
 index = lengths_list.index(max_value)
 filter_value = int(index) * 10
 
+# print result
 print(f"The best filter value is: {filter_value}")
 
