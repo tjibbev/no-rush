ca76623bb3e49d9ca18da567314aa2cff560d971
Doena van den Hoven <doenavandenhoven@MacBook-van-Doena.local>
2022-02-01 15:53:50 +0100

    auto_random_run.py comitted

--- auto_random_run.py
+++ auto_random_run.py
@@ -2,12 +2,14 @@ import subprocess
 import time
 import csv
 
+
 start = time.time()
 n_runs = 0
 
-while time.time() - start < 3600:
+while time.time() - start < 30:
     print(f"run: {n_runs}")
-    f = open("results.txt", "a")
+    f = open("results_random.txt", "a")
 
-    subprocess.call(["timeout", "60", "python3", "main.py", "Rushhour6x6_2", "r", "-no-gif"], stdout=f)
+    subprocess.call(["timeout", "5", "python3", "main.py", "Rushhour6x6_2", "r", "--no-gif"], stdout=f)
     n_runs += 1
+f.close()
